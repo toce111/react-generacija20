9afd07f7aa49b405a9ac267dd955e737bd2fd8dc
toce111 <nestorovantonio@icloud.com>
2022-12-26 23:30:10 +0100

    Homework {first task} completed.

---
+++ src/components/LifecycleClassComponent.jsx
@@ -0,0 +1,61 @@
+import { Component } from "react";
+
+
+export default class LifecycleClassComponent extends Component {
+
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            counter : 8
+        }
+
+        console.log("LifecycleClassComponent => constructor" )
+    }
+
+    componentDidMount() {
+        console.log("LifecycleClassComponent => componentDidMount" )
+    }
+
+    componentWillUnmount(){
+        console.log("LifecycleClassComponent => componentWillUnmount" )
+    }
+
+    increment = () => {
+        console.log("increment" )
+        // this.state.counter = 20
+        this.setState((state) => ({
+            counter : state.counter + 10
+        }))
+    }
+
+    decrement = () => {
+        this.setState((state) =>({
+            counter : state.counter - 10
+        }))
+    }
+
+
+    render() {
+
+        console.log("LifecycleClassComponent => render" )
+
+        return <div>
+            <hr />
+            Class Component
+            <br />
+            counter : {this.state.counter}
+
+            <br />
+            <button
+                onClick={this.increment}
+            >Increment</button>
+            <button
+                onClick={this.decrement}
+            >Decreament</button>
+        </div>
+    }
+}
+
+// Task one in LifecycleClassComponent is finished.
---
+++ src/components/LifecycleFuncComponent.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react"
+
+
+const LifecycleFuncComponent = () => {
+
+    const [counter, setCounter] = useState(7)
+
+    const [color, setColor] = useState("brown")
+    // let counter = 7
+
+    const increment = () => {
+        setCounter(counter + 10)
+    }
+
+    const decreament = () => {
+        setCounter(counter - 10)
+    }
+
+    console.log("render")    
+    console.log("counter", counter)    
+
+    
+
+    return <div>
+        counter : {counter}
+
+        <br />
+        <button onClick={increment}>Increment</button>
+        <button 
+            onClick={() => setCounter(counter + 10)}
+        >Increment v2</button>
+        <button onClick={decreament}>Decreament</button>
+
+        <hr />
+
+        <div style={{
+            color: color
+        }}>Text With Color</div>
+
+        <button 
+            onClick={() => setColor("green")}
+        >change to green</button>
+        
+        <button 
+            onClick={() => setColor("red")}
+        >change to red</button>
+
+    </div>
+}
+
+// Task one in LifecycleFuncComponent is finished.    
+
+export default LifecycleFuncComponent
